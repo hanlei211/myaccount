95ff5d854c2ea09cc5e026c247e9753cdb309072
hanlei <463779213@qq.com>
2019-09-03 17:54:36 +0800

    first commit

---
+++ src/components/views/login.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="login">
+            <div class="login-title">记账管理系统</div>
+            <el-form :model="param" :rules="rules" ref="login" >
+                <el-form-item prop="userName">
+                   <label class="login-label" style="width:80px">用户名</label>
+                   <div class="login-content">
+                        <el-input  v-model="param.userName" aria-placeholder="请输入用户名">
+                             
+                         </el-input> 
+                   </div>
+                  
+                </el-form-item>
+                <el-form-item prop="passWord">
+                  <label class="login-label">密码</label>
+                  <div class="login-content">
+                       <el-input class="login-content" type="password" v-model="param.passWord" aria-placeholder="请输入密码">
+                     
+                    </el-input>
+                   </div>
+                    
+                </el-form-item>
+                <div class="login-btn">
+                    <el-button type="primary" @click="submitLogin()">登录</el-button>
+                </div>
+            </el-form>
+        
+    </div>
+
+</template>
+
+    <script>
+        export default{
+            data:function(){
+              return{
+                param: {
+                  userName: 'admin',
+                  passWord:'123456',
+                        },
+                rules:{
+                    userName:[{required:true,message:'请输入用户名'}],
+                    passWord:[{required:true,message:'请输入密码'}],
+                }        
+              } 
+            },
+            methods:{
+                submitLogin(){
+                   this.$refs.login.validate(valid =>{
+                       if(valid){
+                           this.$message.success("登陆成功");
+                           localStorage.setItem("userName",this.param.userName);
+                           this.$router.push("/");
+                       }else{
+                           this.$message("请输入用户名和密码");
+                           console.info("error !");
+                           return false;
+                       }
+                   }) ;
+                }
+            }
+        }
+    </script>
