3e42c040addc755631615da67655f07fe394bdfb
[hanlei211] <[463779213@qq.com]>
2019-09-09 23:53:46 +0800

    aaa

--- src/components/common/home.vue
+++ src/components/common/home.vue
@@ -1,19 +1,49 @@
 <template>
-  <div class="home">
+<div class="home">
     <v-header></v-header>
-    <v-sliderbar></v-sliderbar>
-  </div>
+    <el-row :gutter="24" class="home-row">
+        <el-col :span="3" class="slider">
+            <v-sliderbar></v-sliderbar>
+        </el-col>
+        <el-col :span="21">
+            <div class="grid-content bg-purple"></div>
+        </el-col>
+    </el-row>
+</div>
 </template>
+
 <script>
 import vHeader from './Header.vue';
 import bus from './Bus';
 import vSliderbar from './Sliderbar.vue';
 export default {
-  data () {
-    return {};
-  },
-  components: {
-    vHeader, vSliderbar
-  }
+    data() {
+        return {};
+    },
+    components: {
+        vHeader,
+        vSliderbar
+    }
 }
 </script>
+
+<style scoped>
+.home {
+    width: 100%;
+    height: 100%;
+}
+
+.home-row {
+    height: 100%;
+}
+
+.home-row /deep/ .slider {
+    padding-right: 0px !important;
+    padding-left: 0px !important;
+}
+
+.slider {
+    background-color: #424f63;
+    height: 100%;
+}
+</style>
